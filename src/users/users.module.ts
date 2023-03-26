4d76de63341473398f6fc2933fbacc5a44daf4e6
ilmar <ilmar.danilo@querodelivery.com>
2023-03-26 16:24:03 -0300

    feat: implement create user in users service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from '../prisma/prisma.service';
 import { UsersRepository } from './users.repository';
+import { HashService } from '../security/hash/hash.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, PrismaService],
+  providers: [UsersService, UsersRepository, PrismaService, HashService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersRepository } from './users.repository';
+import { HashService } from '../security/hash/hash.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly hashService: HashService,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, senha } = createUserDto;
+
+    const user = await this.usersRepository.findOne({ email });
+
+    if (user) {
+      throw new UnprocessableEntityException(
+        'Já existe um usuário com esse email.',
+      );
+    }
+
+    const passwordHash = await this.hashService.generateHash(senha);
+
+    return this.usersRepository.create({
+      ...createUserDto,
+      senha: passwordHash,
+    });
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  async update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} user`;
   }
 }
